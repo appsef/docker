2847602e07fb4615dca03efa5b462fdda0f0e1e6
Alok J Kashyap <alokjkashyap@gmail.com>
2020-12-31 14:20:27 +0530

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bingo! It worked');
+  res.send('Bingo! It worked and now updated');
 });
 
 app.get('/trigger', (req, res) => {
